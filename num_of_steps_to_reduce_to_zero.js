43543648eba0762d36ba926b6af1c9c83834e4b3
Rocco Lattanzio <lukelattanzio@yahoo.com>
2024-03-14 18:47:11 -0700

    added num of steps to reduce to zero

---
+++ num_of_steps_to_reduce_to_zero.js
@@ -0,0 +1,13 @@
+var numberOfSteps = function(num) {
+    let count = 0;
+    while (num > 0) {
+        if (num % 2 === 0) {
+            num /= 2;
+            count++;
+        } else {
+            num -= 1;
+            count++;
+        }
+    }
+    return count;
+};
